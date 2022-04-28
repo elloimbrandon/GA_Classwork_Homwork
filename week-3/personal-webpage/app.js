eaaf16dc72a0326c42d35ed8312cd50638f2cb97
Brandon Feltz <elloimbrandon@gmail.com>
2022-04-28 10:03:19 -0700

    finished nav bar

--- week-3/personal-webpage/app.js
+++ week-3/personal-webpage/app.js
@@ -1,3 +1,8 @@
 console.log("app.js linked.");
 
-$(() => {});
+$(() => {
+  $(".burger-svg").on("click", function () {
+    $(".burger-bar-ol").toggle(150).css("display", "flex");
+    $(".burger-svg").toggleClass("burger-svg-close");
+  });
+});
