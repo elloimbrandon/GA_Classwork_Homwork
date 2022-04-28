12079e78a540e818345b5bb8222bfe4dd13fca32
Brandon Feltz <elloimbrandon@gmail.com>
2022-04-28 12:35:04 -0700

    working on bio

--- week-3/personal-webpage/app.js
+++ week-3/personal-webpage/app.js
@@ -1,6 +1,9 @@
 console.log("app.js linked.");
 
 $(() => {
+  //   $("body").hide();
+  //   $("body").show(1500);
+
   $(".burger-svg").on("click", function () {
     $(".burger-bar-ol").toggle(150).css("display", "flex");
     $(".burger-svg").toggleClass("burger-svg-close");
