4c9bf0fc086481a4254325c520b0e327a8976bf9
Brandon Feltz <elloimbrandon@gmail.com>
2022-04-29 16:20:05 -0700

    finished project images and jquery, working on resume page now

--- week-3/personal-webpage/app.js
+++ week-3/personal-webpage/app.js
@@ -1,5 +1,7 @@
 console.log("app.js linked.");
 
+// remember to fix styling issue with vh on main container
+
 $(() => {
   //   $("body").hide();
   //   $("body").show(1500);
@@ -7,5 +9,30 @@ $(() => {
   $(".burger-svg").on("click", function () {
     $(".burger-bar-ol").toggle(150).css("display", "flex");
     $(".burger-svg").toggleClass("burger-svg-close");
+  });
+
+  // the functions for hover gif projects
+  $("#minishell-project").mouseenter(function () {
+    $("#minishell-project").css("content", "url(minishell-png.gif)");
+  });
+
+  $("#minishell-project").mouseleave(function () {
+    $("#minishell-project").css("content", "url(minishell-png.jpg)");
+  });
+
+  $("#rps-project").mouseenter(function () {
+    $("#rps-project").css("content", "url(rps1.gif)");
+  });
+
+  $("#rps-project").mouseleave(function () {
+    $("#rps-project").css("content", "url(rps.jpg)");
+  });
+
+  $("#wolf3d-project").mouseenter(function () {
+    $("#wolf3d-project").css("content", "url(wolf3d.gif)");
+  });
+
+  $("#wolf3d-project").mouseleave(function () {
+    $("#wolf3d-project").css("content", "url(wolf3d.jpg)");
   });
 });
